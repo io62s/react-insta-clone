429d9d5a658d3104e737419bf3cc43c8caae539f
io62s <ufo62s@gmail.com>
2021-05-16 22:13:20 +0200

    post styles updt

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -5,19 +5,25 @@ import "./PostStyle.css";
 function Post() {
   return (
     <div className="post">
-      <Avatar
-        className="post__avatar"
-        alt="user-avatar"
-        src="/static/images/avatar/1.jpg"
-      />
-      <h3>Ganimed</h3>
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="Ganimed-avatar"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>Ganimed</h3>
+      </div>
       <img
         src="https://nerdist.com/wp-content/uploads/2020/04/DC-Comics-New-Comics-Header.jpg"
         alt="post"
         className="post__image"
       />
       <h4>
-        Ganimed: <span>caption</span>
+        Ganimed:{" "}
+        <span>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda,
+          odio?
+        </span>
       </h4>
     </div>
   );
