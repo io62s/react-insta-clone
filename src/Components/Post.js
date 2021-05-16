d1eb339be6553ff582e454657bca4e98f03d2068
io62s <ufo62s@gmail.com>
2021-05-16 21:56:07 +0200

    minor updt

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 import "./PostStyle.css";
 
 function Post() {
   return (
     <div className="post">
+      <Avatar
+        className="post__avatar"
+        alt="user-avatra"
+        src="/static/images/avatar/1.jpg"
+      />
       <h3>Ganimed</h3>
       <img
         src="https://nerdist.com/wp-content/uploads/2020/04/DC-Comics-New-Comics-Header.jpg"
